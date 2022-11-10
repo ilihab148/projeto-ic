840530a10875c9c3cedecc8a9283bb189eb05a3b
ilihab148 <48345664+ilihab148@users.noreply.github.com>
2022-11-10 23:34:53 +0000

    Add files via upload

---
+++ IC website/js/comportamentoIA.js
@@ -0,0 +1,11 @@
+"use strict"
+let all_check = document.getElementById("all")
+let lista_acompanhamentos_check = document.getElementsByName("acompanhamento")
+for (let i = 0; i<4; i++){
+    console.log(lista_acompanhamentos_check[i]);
+}
+function checks_all_button(){
+    if (all_check.checked == true){
+
+    }
+}
